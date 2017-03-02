10b5b7f492614411d00ce5638b06ba50a2a4d80c
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2017-03-03 00:59:59 +0100

    technically  it's not minimal but whatever

--- samples/minimal-jetpad/src/main/java/jetbrains/jetpad/samples/minimal/mappers/SomeViewMapper.java
+++ samples/minimal-jetpad/src/main/java/jetbrains/jetpad/samples/minimal/mappers/SomeViewMapper.java
@@ -1,10 +1,16 @@
 package jetbrains.jetpad.samples.minimal.mappers;
 
+import com.google.gwt.dom.client.InputElement;
 import jetbrains.jetpad.mapper.Mapper;
 import jetbrains.jetpad.mapper.Synchronizers;
+import jetbrains.jetpad.model.property.Property;
+import jetbrains.jetpad.model.property.WritableProperty;
 import jetbrains.jetpad.samples.minimal.models.SomeModel;
 import jetbrains.jetpad.samples.minimal.views.SomeView;
 
+import javax.lang.model.element.Element;
+
+import static jetbrains.jetpad.mapper.gwt.DomUtil.checkbox;
 import static jetbrains.jetpad.mapper.gwt.DomUtil.innerTextOf;
 import static jetbrains.jetpad.mapper.gwt.DomUtil.hasClass;
 
@@ -14,11 +20,22 @@ public class SomeViewMapper extends Mapper<SomeModel, SomeView> {
         super(source, new SomeView());
     }
 
+    private static WritableProperty<Boolean> flagDescription(com.google.gwt.dom.client.Element el) {
+        return new WritableProperty<Boolean>() {
+            @Override
+            public void set(Boolean flag) {
+                el.setInnerText(String.valueOf(flag));
+            }
+        };
+    }
+
     @Override
     protected void registerSynchronizers(SynchronizersConfiguration conf) {
         super.registerSynchronizers(conf);
 
         conf.add(Synchronizers.forPropsOneWay(getSource().text, innerTextOf(getTarget().text)));
+        conf.add(Synchronizers.forPropsTwoWay(getSource().flag, checkbox(getTarget().flag)));
+        conf.add(Synchronizers.forPropsOneWay(getSource().flag, flagDescription(getTarget().flagDescription)));
     }
 }
 
--- samples/minimal-jetpad/src/main/java/jetbrains/jetpad/samples/minimal/views/SomeView.java
+++ samples/minimal-jetpad/src/main/java/jetbrains/jetpad/samples/minimal/views/SomeView.java
@@ -3,6 +3,7 @@ package jetbrains.jetpad.samples.minimal.views;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.InputElement;
 import com.google.gwt.thirdparty.common.css.compiler.gssfunctions.GssFunctions;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -19,6 +20,12 @@ public class SomeView extends BaseWithElement{
 
     @UiField
     public DivElement text;
+
+    @UiField
+    public InputElement flag;
+
+    @UiField
+    public  Element flagDescription;
 
     interface SomeViewUiInterface extends UiBinder<Element, SomeView> {}
 }
